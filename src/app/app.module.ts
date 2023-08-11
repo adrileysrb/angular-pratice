531a5c2975058c6d4643d3a274bed85765a78f37
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2023-08-11 01:47:32 +0000

    Add some components, config the routes and add services in appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { NotificationService } from './service/notification.service';
+import { AuthenticationService } from './service/authentication.service';
+import { UserService } from './service/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [NotificationService, AuthenticationService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
