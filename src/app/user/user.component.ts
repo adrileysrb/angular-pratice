9970f8ebce6471c4de9f2dc45374444cab06019c
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2023-09-18 10:33:21 +0000

    User edit dialog has implemmented

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -150,4 +150,25 @@ export class UserComponent implements OnInit, OnDestroy {
   onProfileImageChange($event: Event) {
   
   }
+  
+  confirm1() {
+    this.confirmationService.confirm({
+        message: 'Are you sure that you want to proceed?',
+        header: 'Confirmation',
+        icon: 'pi pi-exclamation-triangle',
+        accept: () => {
+            this.messageService.add({ severity: 'success', summary: 'Confirmed', detail: 'You have accepted' });
+        },
+        reject: (type: ConfirmEventType) => {
+            switch (type) {
+              case ConfirmEventType.REJECT:
+                this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
+                break;
+              case ConfirmEventType.CANCEL:
+                this.messageService.add({ severity: 'warn', summary: 'Cancelled', detail: 'You have cancelled' });
+                break;
+            }
+        }
+    });
+  }
 }
